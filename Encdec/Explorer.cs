cc5b63cecb3c32546be2adb32ba4b0ed2177fde4
iswenzz <suxlolz1528@gmail.com>
2019-12-11 16:41:38 +0100

    Explorer: use absolute path for PAK folder

--- Encdec/Explorer.cs
+++ Encdec/Explorer.cs
@@ -9,11 +9,14 @@ namespace Iswenzz.AION.Encdec
 {
     public static class Explorer
     {
+        public static string PAKFolder { get; set; }
+            = Path.Combine(Application.StartupPath, "PAK");
+
         public static string[] GetSelectedFolders()
         {
             List<string> list = new List<string>();
             foreach (object item in (Application.OpenForms[0] as Encdec).listBox.CheckedItems)
-                list.Add("./PAK/" + Path.GetFileNameWithoutExtension((string)item));
+                list.Add(Path.Combine(PAKFolder, Path.GetFileNameWithoutExtension((string)item)));
             return list.ToArray();
         }
 
@@ -21,7 +24,7 @@ namespace Iswenzz.AION.Encdec
         {
             List<string> list = new List<string>();
             foreach (object item in (Application.OpenForms[0] as Encdec).listBox.CheckedItems)
-                list.Add("./PAK/" + (string)item);
+                list.Add(Path.Combine(PAKFolder, (string)item));
             return list.ToArray();
         }
 
@@ -32,14 +35,14 @@ namespace Iswenzz.AION.Encdec
             CheckedListBox listBox = (Application.OpenForms[0] as Encdec).listBox;
             CheckedListBox.ObjectCollection collection = listBox.Items;
 
-            Directory.CreateDirectory("./PAK/");
-
+            Directory.CreateDirectory(PAKFolder);
             while (true)
             {
-                if (collection.Count != Directory.GetFiles("./PAK/", "*.pak").Length)
+                if (collection.Count != Directory.GetFiles(PAKFolder, "*.pak").Length)
                 {
                     listBox.Invoke(new Action(() => collection.Clear()));
-                    IEnumerable<object> paks = Directory.GetFiles("./PAK/", "*.pak").Select(item => item.Replace("./PAK/", ""));
+                    IEnumerable<object> paks = Directory.GetFiles(PAKFolder, "*.pak")
+                        .Select(item => item.Replace(PAKFolder + "\\", ""));
                     listBox.Invoke(new Action(() => collection.AddRange(paks.ToArray())));
                 }
                 Thread.Sleep(1000);

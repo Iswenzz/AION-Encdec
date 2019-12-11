8f899f89a89a5fa669bb09fa975185bf194c1ca3
iswenzz <suxlolz1528@gmail.com>
2019-12-11 16:50:51 +0100

    PopUp: removed unnecesarry functions

--- PopUp.cs
+++ PopUp.cs
@@ -18,24 +18,7 @@ namespace Iswenzz.AION.Encdec
             SetDesktopLocation(x + 410, y + 190);
         }
 
-        private void bunifuFlatButton4_Click(object sender, EventArgs e) => Hide();
-
-        int X, Y, isMoving;
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) => Process.Start("https://iswenzz.com/");
-        private void panel2_MouseUp(object sender, MouseEventArgs e) => isMoving = 0;
-
-        private void panel2_MouseDown(object sender, MouseEventArgs e)
-        {
-            isMoving = 1;
-
-            X = e.X;
-            Y = e.Y;
-        }
-
-        private void panel2_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isMoving == 1)
-                SetDesktopLocation(MousePosition.X - X, MousePosition.Y - Y);
-        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) 
+            => Process.Start("https://iswenzz.com/");
     }
 }
